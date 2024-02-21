6a3ba1b94046bac823fb5698ee803f5baeb5f637
TJ Murphy <1796+teej@users.noreply.github.com>
2024-02-20 18:50:55 -0800

    role grants

--- main.py
+++ main.py
@@ -14,6 +14,26 @@ def crawl(path: str):
                 yield os.path.join(root, file)
 
 
+def role_grants_from_config(role_grants: list):
+    role_grants = []
+    for role_grant in role_grants:
+        for user in role_grant.get("users", []):
+            role_grants.append(
+                RoleGrant(
+                    role=role_grant["role"],
+                    to_user=user,
+                )
+            )
+        for to_role in role_grant.get("roles", []):
+            role_grants.append(
+                RoleGrant(
+                    role=role_grant["role"],
+                    to_role=to_role,
+                )
+            )
+    return role_grants
+
+
 def collect_resources_from_config(config: dict):
     databases = config.get("databases", [])
     role_grants = config.get("role_grants", [])
@@ -24,7 +44,7 @@ def collect_resources_from_config(config: dict):
     databases = [Database(**database) for database in databases]
     users = [User(**user) for user in users]
     roles = [Role(**role) for role in roles]
-    # role_grants = [RoleGrant(**role_grant) for role_grant in role_grants]
+    role_grants = role_grants_from_config(role_grants)
     warehouses = [Warehouse(**warehouse) for warehouse in warehouses]
 
     return (
