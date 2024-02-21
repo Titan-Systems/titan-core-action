d28fa29c2a84912fc607dc57653c91c8c0deba87
TJ Murphy <1796+teej@users.noreply.github.com>
2024-02-20 17:12:56 -0800

    connect

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ import os
 import snowflake.connector
 
 
+connection_params = {
+    "account": os.environ["SNOWFLAKE_ACCOUNT"],
+    "user": os.environ["SNOWFLAKE_USERNAME"],
+    "password": os.environ["SNOWFLAKE_PASSWORD"],
+    "role": os.environ["SNOWFLAKE_ROLE"],
+    "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
+}
+
+
 def main():
     workspace = os.environ.get("GITHUB_WORKSPACE")
-    print(f"Hello, World! [{workspace}]")
+    conn = snowflake.connector.connect(**connection_params)
+    print(f"Hello, World! [{workspace}]:{conn.role}")
 
 
 if __name__ == "__main__":
