7afe9a5cba73aed085e36611695c1bb31a3a6640
TJ Murphy <1796+teej@users.noreply.github.com>
2024-02-20 18:27:11 -0800

    blueprint

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,8 @@
-FROM python:3.11-slim-bookworm
-
-RUN apt-get -y update; apt-get -y install curl
-
-ENV VIRTUAL_ENV=/usr/local
-ADD --chmod=755 https://astral.sh/uv/install.sh /install.sh
-RUN /install.sh && rm /install.sh
+FROM python:3.11
 
 COPY requirements.txt /requirements.txt
 
-RUN /root/.cargo/bin/uv pip install --no-cache -r requirements.txt
+RUN python -m pip install --no-cache -r requirements.txt
 
 COPY main.py /main.py
 
--- main.py
+++ main.py
@@ -1,6 +1,50 @@
 import os
+import yaml
 
 import snowflake.connector
+
+from titan import Blueprint
+from titan.resources import Database, Warehouse, Role, User, RoleGrant
+
+
+def crawl(path: str):
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(".yaml") or file.endswith(".yml"):
+                yield os.path.join(root, file)
+
+
+def collect_resources_from_config(config: dict):
+    databases = config.get("databases", [])
+    role_grants = config.get("role_grants", [])
+    roles = config.get("roles", [])
+    users = config.get("users", [])
+    warehouses = config.get("warehouses", [])
+
+    databases = [Database(**database) for database in databases]
+    users = [User(**user) for user in users]
+    roles = [Role(**role) for role in roles]
+    # role_grants = [RoleGrant(**role_grant) for role_grant in role_grants]
+    warehouses = [Warehouse(**warehouse) for warehouse in warehouses]
+
+    return (
+        *databases,
+        *role_grants,
+        *roles,
+        *users,
+        *warehouses,
+    )
+
+
+def collect_resources(path: str):
+    resources = []
+
+    for file in crawl(path):
+        with open(file, "r") as f:
+            config = yaml.safe_load(f)
+            resources.extend(collect_resources_from_config(config))
+
+    return resources
 
 
 def main():
@@ -20,10 +64,15 @@ def main():
     except KeyError as e:
         raise ValueError(f"Missing environment variable: {e}") from e
 
-    conn = snowflake.connector.connect(**connection_params)
-    print(f"Hello, World! [{workspace}]:{conn.role}")
+    print(f"Workspace: [{workspace}]")
     print(f"Resource Path: {resource_path}")
     print(f"Allowed Resources: {allowed_resources}")
+
+    resources = collect_resources(os.path.join(workspace, resource_path))
+    blueprint = Blueprint(resources=resources)
+    conn = snowflake.connector.connect(**connection_params)
+    blueprint.plan(conn)
+    blueprint.apply(conn)
 
 
 if __name__ == "__main__":
