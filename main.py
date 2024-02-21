4305cb5c452005d9c3e06ead86f0fec49bde7de3
TJ Murphy <1796+teej@users.noreply.github.com>
2024-02-20 17:47:54 -0800

    ins and outs

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ import os
 import snowflake.connector
 
 
-connection_params = {
-    "account": os.environ["SNOWFLAKE_ACCOUNT"],
-    "user": os.environ["SNOWFLAKE_USERNAME"],
-    "password": os.environ["SNOWFLAKE_PASSWORD"],
-    "role": os.environ["SNOWFLAKE_ROLE"],
-    "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
-}
-
-
 def main():
-    workspace = os.environ.get("GITHUB_WORKSPACE")
+
+    # Bootstrap environment
+    try:
+        connection_params = {
+            "account": os.environ["SNOWFLAKE_ACCOUNT"],
+            "user": os.environ["SNOWFLAKE_USERNAME"],
+            "password": os.environ["SNOWFLAKE_PASSWORD"],
+            "role": os.environ["SNOWFLAKE_ROLE"],
+            "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
+        }
+        workspace = os.environ["GITHUB_WORKSPACE"]
+        resource_path = os.environ["resource-path"]
+        allowed_resources = os.environ.get("allowed-resources", "all")
+    except KeyError as e:
+        raise ValueError(f"Missing environment variable: {e}") from e
+
     conn = snowflake.connector.connect(**connection_params)
     print(f"Hello, World! [{workspace}]:{conn.role}")
 
