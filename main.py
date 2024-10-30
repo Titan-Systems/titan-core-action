2e08e7c0c05507ee5b9581e719ed192f13a36676
TJ Murphy <1796+teej@users.noreply.github.com>
2024-10-30 16:55:23 -0700

    update to v0.10

--- main.py
+++ main.py
@@ -1,54 +1,16 @@
+import json
 import os
-import yaml
-
-import snowflake.connector
 
 from titan import Blueprint
-from titan.blueprint import RunMode, print_plan
-from titan.enums import ResourceType
-from titan.gitops import collect_resources_from_config
-
-
-def crawl(path: str):
-    for root, _, files in os.walk(path):
-        for file in files:
-            if file.endswith(".yaml") or file.endswith(".yml"):
-                yield os.path.join(root, file)
-
-
-def collect_resources(path: str):
-    resources = []
-    files_read = 0
-
-    if os.path.isfile(path):
-        # Handle single file
-        if path.endswith((".yaml", ".yml")):
-            print(f"Reading config file: {path}")
-            with open(path, "r") as f:
-                config = yaml.safe_load(f)
-                if config:
-                    resources.extend(collect_resources_from_config(config))
-                    files_read += 1
-                else:
-                    print(f"Skipping empty config file: {path}")
-    elif os.path.isdir(path):
-        # Handle directory
-        for file in crawl(path):
-            with open(file, "r") as f:
-                print(f"Reading config file: {file}")
-                config = yaml.safe_load(f)
-                if config:
-                    resources.extend(collect_resources_from_config(config))
-                    files_read += 1
-                else:
-                    print(f"Skipping empty config file: {file}")
-    else:
-        raise ValueError(f"Invalid path: {path}. Must be a file or directory.")
-
-    if files_read == 0:
-        raise ValueError(f"No valid YAML files were read from the given path: {path}")
-
-    return resources
+from titan.blueprint import print_plan
+from titan.gitops import (
+    collect_blueprint_config,
+    collect_configs_from_path,
+    collect_vars_from_environment,
+    merge_configs,
+    merge_vars,
+)
+from titan.operations.connector import connect
 
 
 def str_to_bool(s: str) -> bool:
@@ -61,53 +23,63 @@ def str_to_bool(s: str) -> bool:
 def main():
     # Bootstrap environment
     try:
-        connection_params = {
-            "account": os.environ["SNOWFLAKE_ACCOUNT"],
-            "user": os.environ["SNOWFLAKE_USERNAME"],
-            "password": os.environ["SNOWFLAKE_PASSWORD"],
-            "role": os.environ["SNOWFLAKE_ROLE"],
-            "warehouse": os.environ["SNOWFLAKE_WAREHOUSE"],
-        }
         workspace = os.environ["GITHUB_WORKSPACE"]
 
         # Inputs
         run_mode = os.environ["INPUT_RUN-MODE"]
-        dry_run = str_to_bool(os.environ["INPUT_DRY-RUN"])
         resource_path = os.environ["INPUT_RESOURCE-PATH"]
         allowlist = os.environ.get("INPUT_ALLOWLIST", "all")
+        vars = os.environ.get("INPUT_VARS", None)
+        dry_run = str_to_bool(os.environ["INPUT_DRY-RUN"])
+        scope = os.environ.get("INPUT_SCOPE", None)
+        database = os.environ.get("INPUT_DATABASE", None)
+        schema = os.environ.get("INPUT_SCHEMA", None)
     except KeyError as e:
         raise ValueError(f"Missing environment variable: {e}") from e
 
-    # Parse inputs
-    run_mode = RunMode(run_mode)
+    action_config = {
+        "run_mode": run_mode,
+        "resource_path": resource_path,
+        "allowlist": allowlist,
+        "vars": json.loads(vars) if vars else None,
+        "dry_run": dry_run,
+        "scope": scope,
+        "database": database,
+        "schema": schema,
+    }
 
-    if allowlist == "all":
-        allowlist = []
-    else:
-        allowlist = [ResourceType(r) for r in allowlist.split(",")]
+    env_vars = collect_vars_from_environment()
+    if env_vars:
+        action_config["vars"] = merge_vars(action_config.get("vars", {}), env_vars)
 
-    # Print config
-    print("Config\n------")
+    print("Configuration\n------")
     print(f"\t run_mode: {run_mode}")
-    print(f"\t allowlist: {allowlist}")
-    print(f"\t dry_run: {os.environ['INPUT_DRY-RUN']} => {dry_run}")
     print(f"\t resource_path: {resource_path}")
+    print(f"\t allowlist: {allowlist}")
+    print(f"\t dry_run: {dry_run}")
+    print(f"\t scope: {scope}")
+    print(f"\t database: {database}")
+    print(f"\t schema: {schema}")
     print(f"\t workspace: {workspace}")
 
-    resources = collect_resources(os.path.join(workspace, resource_path))
+    print("Vars\n------")
+    for key in action_config["vars"].keys():
+        print(f"\t {key}")
 
-    blueprint = Blueprint(
-        name="snowflake-gitops",
-        resources=resources,
-        run_mode=run_mode,
-        dry_run=dry_run,
-        allowlist=allowlist,
-    )
-    print(resources)
-    conn = snowflake.connector.connect(**connection_params)
-    plan = blueprint.plan(conn)
+    configs = collect_configs_from_path(os.path.join(workspace, resource_path))
+    yaml_config = {}
+
+    print("Resource files\n------")
+    for config in configs:
+        print(f"\t{config[0]}")
+        yaml_config = merge_configs(yaml_config, config[1])
+
+    blueprint_config = collect_blueprint_config(yaml_config, action_config)
+    blueprint = Blueprint.from_config(blueprint_config)
+    session = connect()
+    plan = blueprint.plan(session)
     print_plan(plan)
-    blueprint.apply(conn, plan)
+    blueprint.apply(session, plan)
 
 
 if __name__ == "__main__":
